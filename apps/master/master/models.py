3f739840cd4d5a7a13c3db3f497ea339ace0c414
Ehab-24 <ehabs1775@gmail.com>
2024-06-04 14:29:32 +0500

    feat(MA): add checksum to chunks

--- apps/master/master/models.py
+++ apps/master/master/models.py
@@ -32,6 +32,19 @@ class ChunkServer(models.Model):
         self.save()
         return response
 
+    def normalize(self, value, min_value, max_value):
+        if max_value == min_value:
+            return 1.0
+        return (value - min_value) / (max_value - min_value)
+
+    def compute_score(self, weights, min_max_values):
+        cpu_usage = self.normalize(self.stats['cpu']['usage'][0], min_max_values['cpu']['min'], min_max_values['cpu']['max'])
+        disk_usage = self.normalize(self.stats['disk']['used'], min_max_values['disk']['min'], min_max_values['disk']['max'])
+        memory_usage = self.normalize(self.stats['memory']['used'], min_max_values['memory']['min'], min_max_values['memory']['max'])
+        score = (weights['cpu'] * cpu_usage) + (weights['disk'] * disk_usage) + (weights['memory'] * memory_usage)
+        return score
+
+
 class Video(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=100)
@@ -48,6 +61,7 @@ class Chunk(models.Model):
     end_time = models.IntegerField()
     replicas = models.ManyToManyField(ChunkServer)
     n_replicas = models.IntegerField()
+    checksum = models.CharField(max_length=60)
 
     def assign_replicas(self, servers):
         """
@@ -65,11 +79,45 @@ class ChunkCreator:
         self.servers = servers
         self.video = video
         self.chunk_count = chunk_count
+        self.weights = {
+            'cpu': 0.5,
+            'disk': 0.3,
+            'memory': 0.2
+        }
 
-    def select_chunk_server(self):
-        server = random.choice(self.servers)
-        self.servers.remove(server)
-        return server
+    def get_min_max_values(self):
+        min_max_values = {
+            'cpu': {'min': float('inf'), 'max': float('-inf')},
+            'disk': {'min': float('inf'), 'max': float('-inf')},
+            'memory': {'min': float('inf'), 'max': float('-inf')}
+        }
+        
+        for server in self.servers:
+            cpu_usage = server.stats['cpu']['usage'][0]
+            disk_used = server.stats['disk']['used']
+            memory_used = server.stats['memory']['used']
+            
+            if cpu_usage < min_max_values['cpu']['min']:
+                min_max_values['cpu']['min'] = cpu_usage
+            if cpu_usage > min_max_values['cpu']['max']:
+                min_max_values['cpu']['max'] = cpu_usage
+            
+            if disk_used < min_max_values['disk']['min']:
+                min_max_values['disk']['min'] = disk_used
+            if disk_used > min_max_values['disk']['max']:
+                min_max_values['disk']['max'] = disk_used
+            
+            if memory_used < min_max_values['memory']['min']:
+                min_max_values['memory']['min'] = memory_used
+            if memory_used > min_max_values['memory']['max']:
+                min_max_values['memory']['max'] = memory_used
+        
+        return min_max_values
+
+    def select_chunk_servers(self, count):
+        min_max_values = self.get_min_max_values()
+        server_scores = [(server, server.compute_score(self.weights, min_max_values)) for server in self.servers]
+        return [server for server, _ in sorted(server_scores, key=lambda x: x[1], reverse=True)[:count]]
 
     def create_chunks(self, chunk_duration):
         chunks = []
@@ -83,7 +131,7 @@ class ChunkCreator:
                 n_replicas=self.video.replication_factor
             )
             initial_servers = deepcopy(self.servers)
-            chunk_servers = [self.select_chunk_server() for _ in range(self.video.replication_factor)]
+            chunk_servers = self.select_chunk_servers(self.video.replication_factor)
 
             self.servers = initial_servers
             chunk.assign_replicas(chunk_servers)
